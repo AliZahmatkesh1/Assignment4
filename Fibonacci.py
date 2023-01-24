77b97346eab2153f68afd3ca3bba4e5163b903c1
alizahmatkesh <51386194+AliZahmatkesh1@users.noreply.github.com>
2023-01-24 04:58:03 +0330

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+while True :
+    print ("Exit = exit")
+    n = input("How many Fibonacci sequences do you want? \n_ ").strip().lower()
+    if n == "exit" :
+        break
+    else :
+        n = int(n)
+
+        fib = [0,1]
+        a , b = 0 , 1
+        for i in range(1 , n - 1) :
+            fib.append(fib[a] + fib[b])
+            a += 1
+            b += 1
+        print(fib)
+        for s in fib :
+            print(s)
